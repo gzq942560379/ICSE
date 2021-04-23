b82a5f853fc77c4fbfba7e837315a90806099ff2
gzq <gzq9425@qq.com>
2021-04-23 02:33:30 +0000

    1

--- exp_4/exp_4_2_fast_style_transfer_infer_student/src/utils.py
+++ exp_4/exp_4_2_fast_style_transfer_infer_student/src/utils.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 import scipy.misc, numpy as np, os, sys
 
 def save_img(out_path, img):
@@ -12,11 +13,12 @@ def scale_img(style_path, style_scale):
     style_target = _get_img(style_path, img_size=new_shape)
     return style_target
 
-def get_img(src, img_size=False):
+def get_img(src, img_size=None):
     #TODO: 使用 scipy.misc 模块读入输入图像 src 并转化成’RGB’ 模式，返回 ndarray 类型数组 img
-    img = ______________________
-    ______________________
-    return img
+    image = scipy.misc.imread(src,mode='RGB')
+    if img_size != None:
+        image = scipy.misc.imresize(image,img_size)
+    return image
 
 def exists(p, msg):
     assert os.path.exists(p), msg
